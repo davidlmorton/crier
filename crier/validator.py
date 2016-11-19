3fd142d284e3316048344ed81c98ae37ca7e0d63
David Morton <davidlmorton@gmail.com>
2016-11-19 02:49:44 +0000

    Support "repeat" script directive

--- crier/validator.py
+++ crier/validator.py
@@ -8,20 +8,30 @@ SCHEMA = """
     "description": "Script a crier webserver.",
     "definitions": {
         "response": {
-            "type": "object"
+            "type": "object",
+            "description": "JSON serializable data to respond with"
         },
         "status_code": {
-            "type": "integer"
+            "type": "integer",
+            "description": "HTTP status code to respond with"
         },
         "headers": {
-            "type": "object"
+            "type": "object",
+            "description": "Headers to respond with"
+        },
+        "repeat": {
+            "type": "integer",
+            "minimum": -1,
+            "default": 0,
+            "description": "Number of times to repeat this script, -1 means repeat forever"
         },
         "script": {
             "type": "object",
             "properties": {
                 "response": { "$ref": "#/definitions/response"},
                 "status_code": { "$ref": "#/definitions/status_code"},
-                "headers": { "$ref": "#/definitions/headers"}
+                "headers": { "$ref": "#/definitions/headers"},
+                "repeat": { "$ref": "#/definitions/repeat"}
             },
             "requiredProperties": [
                 "status_code"
@@ -33,7 +43,7 @@ SCHEMA = """
     "items": { "$ref": "#/definitions/script" },
     "minItems": 1
 }
-"""
+"""  # noqa
 
 
 def _load_schema():
